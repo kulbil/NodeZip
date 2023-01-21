2edc051e7997ac4a8b1502c9027771a6280711a1
kulbil <martin.austvold@gmail.com>
2023-01-21 18:47:12 +0100

    fsf

--- zip.js
+++ zip.js
@@ -4,7 +4,6 @@ function stringRead(str) {
   var newString = "";
   var k = 1;
   for (var i = 0; i < str.length; i++) {
-<<<<<<< HEAD:zip.js
     if (str.charAt(i) == str.charAt(i - 1)) {
       k++
       if (str.charAt(i + 1) == " " || str.charAt(i + 1) == "") {
@@ -12,19 +11,6 @@ function stringRead(str) {
         k = 1;
       }
     }
-=======
-    stringArray.push(str.charAt(i));
-  }
-
-  for (var j = 0; j < stringArray.length; j++) {
-    if(stringArray[j] == " " || j == stringArray.length - 1) {
-      newString += (k - 1) + stringArray[j - 1] + stringArray[j];
-      k = 0;
-      console.log(newString); 
-    }
-    k++
-    
->>>>>>> 3536ef2495787049d6e18427571cf99c62f050c2:script.js
   }
   return newString;
 }
